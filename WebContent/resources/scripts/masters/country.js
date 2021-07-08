b8a37628ccb6e32ee5d49903589c1bcc231fae56
Ramakrishna <Ramakrishna@LAPTOP-1ANEFM99>
2021-07-09 02:13:10 +0530

    contry screen. modify and delete functions added

--- WebContent/resources/scripts/masters/country.js
+++ WebContent/resources/scripts/masters/country.js
@@ -4,19 +4,25 @@
 
 $(document).ready(function(){
  showCountries();
+ 
+
 });
 
 function showCountries(){
 	countryService.getAllCountries(
 			
 	  function(countriesList){
-		  debugger
 		  $("#countriesListTable").empty();
 		  if(countriesList.length>0){
-			  var tableHeaderContent="<thead ><tr><th>Country</th><th>Last Update</th><th>Action</th></tr></thead>";
+			  var tableHeaderContent="<thead class='thead-light' ><tr><th>Country</th><th>Last Update</th><th>Action</th></tr></thead>";
 			  var tableBodyContent="<tbody>";
 			  for(var i=0;i<countriesList.length;i++){
-				  tableBodyContent+="<tr id="+countriesList[i].countryId+" ><td>"+countriesList[i].country+"</td><td>"+countriesList[i].lastUpdate+"</td><td></td></tr>";
+				  tableBodyContent+="<tr id="+countriesList[i].countryId+" >" +
+				  	                "<td>"+countriesList[i].country+"</td>" +
+				  				     "<td>"+countriesList[i].lastUpdate+"</td>" +
+				  				     "<td><img data-toggle='modal' data-target='#myModal' onclick='showDetails(this);'  style='height: 20px;' src='"+strContextPath+"/resources/images/edit-solid.svg'>" +
+				  				     "<img data-toggle='modal'  data-target='#myConfirmModal' onclick='confirmDelete(this);' style='height: 20px;margin-left:5px;'  src='"+strContextPath+"/resources/images/trash-alt-regular.svg'></td>" +
+				  				 "</tr>";
 			  }
 			  tableBodyContent+="</tbody>";
 			  var tableFootContent="<tfoot><tr><td></td><td></td><td></td></tr></tfoot>";
@@ -29,3 +35,56 @@ function showCountries(){
 	  }
 	  );
 }
+function showDetails(obj){
+	var countryId=$(obj).parents("tr").eq(0).attr("id");
+	var country=$(obj).parents("tr").eq(0).children('td').eq(0).text();
+	var lastUpdate=$(obj).parents("tr").eq(0).children('td').eq(1).text();	
+	$("#modalbody").find("#countryId").val(countryId);
+	$("#modalbody").find("#country").val(country);	
+	$("#modalbody").find("#lastupdate").val(lastUpdate);	
+}
+function modifyCountry(obj){
+	var countryId=$("#modalbody").find("#countryId").val();
+	var country=$("#modalbody").find("#country").val();	
+	var countrObj={countryId:countryId,country:country};
+	countryService.updateCountry(countrObj,{callback:function(resultObj){
+		if(resultObj!=null){
+		   $("#countriesListTable").find("tr[id='"+resultObj.countryId+"']").children("td").eq(0).text(resultObj.country).children("td").eq(1).text(resultObj.lastUpdate);
+		   $("#successDiv").find("#successMsg").text("Successfully modified.")
+		   $("#successDiv").removeAttr('class').attr("class","alert alert-success");
+		}else{
+			$("#errorDiv").find("#errorMsg").text("Unable to modify this country.");
+			$("#errorDiv").removeAttr('class').attr("class","alert alert-danger");
+		}
+		
+	},async:true});
+	
+}
+function deleteCountry(btnObj){
+	var countryId=$(btnObj).attr("countryId");
+	var obj=$("#countriesListTable").find("tr[id='"+countryId+"']");
+	var country=$(obj).children('td').eq(0).text();
+	var lastUpdate=$(obj).children('td').eq(1).text();	
+	var countrObj={countryId:countryId,country:country};
+	countryService.deleteCountry(countrObj,{callback:function(isDeleted){
+		if(isDeleted==true){
+			$("#countriesListTable").find("tr[id='"+resultObj.countryId+"']").remove();	
+			$("#successDiv").find("#successMsg").text("Successfully deleted.")
+			$("#successDiv").removeAttr('class').attr("class","alert alert-success");
+		}else{
+			$("#errorDiv").find("#errorMsg").text("Unable to delete this country.")
+			$("#errorDiv").removeAttr('class').attr("class","alert alert-danger");
+		}
+		
+		
+	},async:true});
+	
+}
+function confirmDelete(obj){
+	var countryId=$(obj).parents("tr").eq(0).attr("id");
+	$("#myConfirmModal").find("#myConfirmModalTitle").text("Are you sure?");
+	$("#myConfirmModal").find("#myConfirmModalBody").text("Do you want to delete this Country.");
+	$("#myConfirmModal").find("#myConfirmModalFooter").find("#confirmYesBtn").removeAttr("onclick").attr("onclick","deleteCountry(this);").removeAttr("countryId").attr("countryId",countryId);
+}
+
+	
--- src/com/dvdrental/inventory/service/CountryManager.java
+++ src/com/dvdrental/inventory/service/CountryManager.java
@@ -1,23 +1,21 @@
 package com.dvdrental.inventory.service;
 
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
 
-import org.directwebremoting.WebContext;
 import org.directwebremoting.WebContextFactory;
 import org.directwebremoting.annotations.RemoteMethod;
 import org.directwebremoting.annotations.RemoteProxy;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.dvdrental.inventory.common.DBFunctions;
 import com.dvdrental.inventory.common.LoggerHelper;
 import com.dvdrental.inventory.common.SpringBeanConstants;
 import com.dvdrental.inventory.dao.CountryDao;
-import com.dvdrental.inventory.dao.impl.CountryDaoImpl;
 import com.dvdrental.inventory.model.Country;
 
 @RemoteProxy(name="countryService")
@@ -30,16 +28,16 @@ public class CountryManager {
 		return country;
 	}
 	public void addCountry() {
-		
-	//WebContext webContext= WebContextFactory.get();
-	ServletContext servletContext= WebContextFactory.get().getServletContext();
-	DBFunctions dbFunctions=new DBFunctions();
-	CountryDao countryDao=(CountryDao) dbFunctions.getDaoImplBean(SpringBeanConstants.CountryDaoImpl, servletContext);
+
+		//WebContext webContext= WebContextFactory.get();
+		ServletContext servletContext= WebContextFactory.get().getServletContext();
+		DBFunctions dbFunctions=new DBFunctions();
+		CountryDao countryDao=(CountryDao) dbFunctions.getDaoImplBean(SpringBeanConstants.CountryDaoImpl, servletContext);
 		Country country= countryDao.loadCountry(3);
 		List<Map<String, Object>> countriesList=countryDao.loadAllCountries();
 
 	}
-	
+
 	@RemoteMethod
 	public List<Country> getAllCountries() {
 		//WebContext webContext= WebContextFactory.get();
@@ -53,23 +51,108 @@ public class CountryManager {
 
 		List<Country> countiesList=null;
 		try {
-		countiesList= countryDao.getAllCountries();
+			countiesList= countryDao.getAllCountries();
 		}catch(Exception e) {
 			e.printStackTrace();
 			if(LoggerHelper.isDebugEnabled()) {
 				LoggerHelper.logDebug(this.getClass().getName(), "getAllCountries", "Exception occured...");
 			}
-			
+
 		}finally {
 			dbFunctions=null;
 			countryDao=null;
-			
+
 		}
 		if(LoggerHelper.isInfoEnabled()) {
 			LoggerHelper.logInfo(this.getClass().getName(), "getAllCountries", "Entering the method");
 		}
-			
-	  return countiesList;
+
+		return countiesList;
+	}
+	@RemoteMethod
+	public Country updateCountry(HashMap countryMap) {
+		//WebContext webContext= WebContextFactory.get();
+		LoggerHelper.intialize();
+		if(LoggerHelper.isInfoEnabled()) {
+			LoggerHelper.logInfo(this.getClass().getName(), "updateCountry", "Entering the method");
+		}
+		Country country=null;
+		int countryId=0;
+		int updatedCountryId=0;
+		ServletContext servletContext= WebContextFactory.get().getServletContext();
+		DBFunctions dbFunctions=new DBFunctions();
+		CountryDao countryDao=(CountryDao) dbFunctions.getDaoImplBean(SpringBeanConstants.CountryDaoImpl, servletContext);
+		try {
+			countryId=Integer.parseInt((String) countryMap.get("countryId"));
+			String countryName=(String)countryMap.get("country");
+			country=countryDao.loadCountry(countryId);
+			if(country!=null) {
+				country.setCountry(countryName);
+				country.setLastUpdate(new Date());
+
+			}
+			updatedCountryId=countryDao.updateCountry(country);
+			if(updatedCountryId==0) {
+				country=null;
+			}
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			if(LoggerHelper.isDebugEnabled()) {
+				LoggerHelper.logDebug(this.getClass().getName(), "updateCountry", "Exception occured...");
+			}
+
+		}finally {
+			dbFunctions=null;
+			countryDao=null;
+
+		}
+		if(LoggerHelper.isInfoEnabled()) {
+			LoggerHelper.logInfo(this.getClass().getName(), "updateCountry", "Entering the method");
+		}
+
+		return country;
+	}
+	@RemoteMethod
+	public boolean deleteCountry(HashMap countryMap) {
+		LoggerHelper.intialize();
+		if(LoggerHelper.isInfoEnabled()) {
+			LoggerHelper.logInfo(this.getClass().getName(), "deleteCountry", "Entering the method");
+		}
+		Country country=null;
+		int countryId=0;
+		int deletedCountryId=0;
+		boolean isdeleted=false;
+		ServletContext servletContext= WebContextFactory.get().getServletContext();
+		DBFunctions dbFunctions=new DBFunctions();
+		CountryDao countryDao=(CountryDao) dbFunctions.getDaoImplBean(SpringBeanConstants.CountryDaoImpl, servletContext);
+		try {
+			countryId=Integer.parseInt((String) countryMap.get("countryId"));
+
+
+			deletedCountryId=countryDao.deleteCountry(countryId);
+			if(deletedCountryId!=0) {
+				isdeleted=true;
+			}else {
+				isdeleted=false;
+			}
+
+		}catch(Exception e) {
+			e.printStackTrace();
+			if(LoggerHelper.isDebugEnabled()) {
+				LoggerHelper.logDebug(this.getClass().getName(), "deleteCountry", "Exception occured...");
+			}
+
+		}finally {
+			dbFunctions=null;
+			countryDao=null;
+
+		}
+		if(LoggerHelper.isInfoEnabled()) {
+			LoggerHelper.logInfo(this.getClass().getName(), "deleteCountry", "Entering the method");
+		}
+
+		return isdeleted;
 	}
 
 
